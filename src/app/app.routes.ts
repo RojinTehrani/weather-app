9883f9049baafae517047f39ffbf7c65e87a581e
RojinTehrani <roji.tehrani@gmail.com>
2026-05-21 23:07:42 +0330

    feat(routing): add dashboard route as default path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { DashboardComponent } from './features/weather/components/dashboard/dashboard.component';
+export const routes: Routes = [
+    {
+        path: '',
+        component: DashboardComponent
+    }
+];
